89ed6746eaf8ba49f26e2f57a1f844cbe7ebaf1c
Akula Shashidhar <mail2shashi11@gmail.com>
2026-08-04 21:38:51 +0530

    Time: 0 ms (100%), Space: 32.2 MB (75.54%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int max1 = *max_element(nums.begin(),nums.end());
-        int min1 = *min_element(nums.begin(),nums.end());
-        unordered_set<int> s(nums.begin(),nums.end());
         vector<int> ans;
-        for(int i=min1; i<=max1; i++){
-            if(!s.count(i)) ans.push_back(i);
+        sort(nums.begin(), nums.end());
+        int idx = 0;
+        for(int i=nums[0]; i<=nums.back(); i++){
+            if(i == nums[idx]) idx++;
+            else ans.push_back(i);
         }
         return ans;
     }

f70b45f53c646effed12738b04c1f08b49551059
Akula Shashidhar <mail2shashi11@gmail.com>
2026-08-21 15:14:46 +0530

    Time: 7 ms (59.32%), Space: 16.9 MB (17.17%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(nums1[i] == nums2[j]) dp[i][j] = 1 + dp[i+1][j+1];
+                else dp[i][j] = max(dp[i][j+1], dp[i+1][j]);
+            }
+        }
+        return dp[0][0];
+    }
+};
